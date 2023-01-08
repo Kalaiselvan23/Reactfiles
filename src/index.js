0723efc6d14425ed8921d1e02df6c1c4f205e62d
Kalaiselvan <kalaiselvan1623@gmail.com>
2023-01-08 08:06:29 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-// import "react";
+//import "react";
 // import "react-dom";
 import React from "react";
 var ReactDOM = require("react-dom");
+const name = "kalaiselvan";
+var date = new Date();
+var year = date.getFullYear();
 ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
 ReactDOM.render(
   <div>
-    <h1>This is my checklist</h1>
+    <h1>
+      This is {name}'s checklist {year}
+    </h1>
     <ul>
       <li>dsa</li>
       <li>full stack</li>
       <li>app dev</li>
     </ul>
+    <p>Copyrights year {}</p>
   </div>,
   document.getElementById("root")
 );
